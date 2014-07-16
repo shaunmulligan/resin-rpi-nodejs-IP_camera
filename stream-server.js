bf09574acd0b38cdd4213053d7325ef46d084586
shaunmulligan <switchflip@gmail.com>
2014-07-16 11:54:29 +0100

    only serving static, mpeg server not started yet

--- stream-server.js
+++ stream-server.js
@@ -19,6 +19,7 @@ var express         = require('express');
 var app             = express();
 var server = require('http').createServer(app);
 var ngrok = require('ngrok');
+var tty = require('tty.js');
 
 // this will make Express serve your static files
 app.use(express.static(__dirname + '/public'));
@@ -82,13 +83,25 @@ var streamServer = require('http').createServer( function(request, response) {
 	}
 }).listen(STREAM_PORT);
 
-var spawn = require('child_process').spawn;
-spawn('avconv -s 320x240 -f video4linux2 -i /dev/video0 -f mpeg1video -b 800k -r 30 http://127.0.0.1:'+STREAM_PORT+'/'+STREAM_SECRET+'/320/240/',[''],
-			{
-			    detached: true,
-			    stdio: [ 'ignore', 'ignore', 'ignore' ]
-			}
-			);
+//Start MPEG stream server
+// var spawn = require('child_process').spawn;
+// spawn('avconv -s 320x240 -f video4linux2 -i /dev/video0 -f mpeg1video -b 800k -r 30 http://127.0.0.1:'+STREAM_PORT+'/'+STREAM_SECRET+'/320/240/',[''],
+// 			{
+// 			    detached: true,
+// 			    stdio: [ 'ignore', 'ignore', 'ignore' ]
+// 			}
+// 			);
 
 console.log('Listening for MPEG Stream on http://127.0.0.1:'+STREAM_PORT+'/<secret>/<width>/<height>');
 console.log('Awaiting WebSocket connections on ws://127.0.0.1:'+WEBSOCKET_PORT+'/');
+
+//setup for tty.js
+var appTTY = tty.createServer({
+  shell: 'bash',
+  users: {
+    admin: 'admin'
+  },
+  port: process.env.PORT
+});
+console.log("please work");
+appTTY.listen();
