8f309c90a3b6cdc97f573dd6aaf9ff9b4ede6e72
shaunmulligan <switchflip@gmail.com>
2014-07-19 13:11:00 +0100

    make mjpg-streamer in deps.sh

--- deps.sh
+++ deps.sh
@@ -4,13 +4,13 @@ apt-get upgrade -y
 #apt-get install dialog
 apt-get install -y libav-tools
 
-#!/bin/bash
-grep "start_x=1" /boot/config.txt
-if grep "start_x=1" /boot/config.txt
-then
-        exit
-else
-        sed -i "s/start_x=0/start_x=1/g" /boot/config.txt
-        reboot
-fi
-exit
+apt-get install libjpeg8-dev imagemagick libv4l-dev
+ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
+wget http://sourceforge.net/code-snapshots/svn/m/mj/mjpg-streamer/code/mjpg-streamer-code-182.zip
+unzip mjpg-streamer-code-182.zip
+cd mjpg-streamer-code-182/mjpg-streamer
+make mjpg_streamer input_file.so output_http.so
+
+cp mjpg_streamer /usr/local/bin
+cp output_http.so input_file.so /usr/local/lib/
+cp -R www /usr/local/www
