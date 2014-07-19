63f1d87c789a9c42759b1155710ad4aa360c734a
shaunmulligan <switchflip@gmail.com>
2014-07-19 01:49:55 +0100

    added bash commands to enable camera module

--- deps.sh
+++ deps.sh
@@ -3,3 +3,14 @@ apt-get update
 apt-get upgrade -y
 #apt-get install dialog
 apt-get install -y libav-tools
+
+#!/bin/bash
+grep "start_x=1" /boot/config.txt
+if grep "start_x=1" /boot/config.txt
+then
+        exit
+else
+        sed -i "s/start_x=0/start_x=1/g" /boot/config.txt
+        reboot
+fi
+exit
